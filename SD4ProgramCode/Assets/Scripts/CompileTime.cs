ed0dea5ff1ea2a540d52d30059cbecf47cf921fa
mattylenepveu <matthew@lenepveu.com>
2019-11-23 10:32:51 +1100

    Initial commit.

---
+++ SD4ProgramCode/Assets/Scripts/CompileTime.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode] 
+public class CompileTime : MonoBehaviour
+{
+    private double m_dCompileStartTime;
+    private double m_dFinalCompileTime;
+
+    private bool m_bIsCompiling = false;
+
+    private void Update()
+    {
+        if (m_bIsCompiling)
+        {
+            if (!EditorApplication.isCompiling)
+            {
+                m_bIsCompiling = false;
+                CompileFinished();
+            }
+        }
+        else
+        {
+            if (EditorApplication.isCompiling)
+            {
+                m_bIsCompiling = true;
+                CompileStarted();
+            }
+        }
+    }
+
+    private void CompileStarted()
+    {  
+        Debug.Log("Compile started...");
+        m_dCompileStartTime = EditorApplication.timeSinceStartup;
+    }
+
+    private void CompileFinished()
+    {
+        m_dFinalCompileTime = EditorApplication.timeSinceStartup - m_dCompileStartTime;
+        Debug.Log("Compile Finished: " + m_dFinalCompileTime.ToString("F2") + "s");
+    }
+} 
