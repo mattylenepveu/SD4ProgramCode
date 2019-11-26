d81ba0692b9a801987b914fdc6020da9b8420a94
mattylenepveu <matthew@lenepveu.com>
2019-11-26 22:28:29 +1100

    Set up the Math functions in a seperate script and I am now half way through collecting the data for my research assignment.

--- SD4ProgramCode/Assets/Scripts/CompileTime.cs
+++ SD4ProgramCode/Assets/Scripts/CompileTime.cs
@@ -5,6 +5,7 @@
 // Lists all the usings the CompileTime script will need
 using UnityEngine;
 using UnityEditor;
+using System.IO;
 
 // Creates the CompileTime class, which only runs in Unity Editor
 [ExecuteInEditMode] 
@@ -19,13 +20,31 @@ public class CompileTime : MonoBehaviour
     // Bool indicates whether the program is compiling or not 
     private bool m_bIsCompiling = false;
 
+    // Variable is used to access the MathFunctions script
+    private MathFunctions m_mathFunctions;
+
+    // Records the file directory where the script can locate the text document
+    private string m_sDirectory;
+
+    //--------------------------------------------------------------------------------
+    // Function is called when script is first called.
+    //--------------------------------------------------------------------------------
+    private void Start()
+    {
+        // Gets the MathsComponent script from the attached game object
+        m_mathFunctions = GetComponent<MathFunctions>();
+
+        // Initializes the directory to the text file
+        m_sDirectory = "Assets/Resources/data.txt";
+    }
+
     //--------------------------------------------------------------------------------
     // Function is called every frame and updates the scene when called.
     //--------------------------------------------------------------------------------
     private void Update()
     {
         // Checks if compiling bool is true
-        if (m_bIsCompiling)
+        if (m_bIsCompiling)  
         {
             // Detects if the editor application has stopped compiling
             if (!EditorApplication.isCompiling)
@@ -61,7 +80,7 @@ public class CompileTime : MonoBehaviour
         Debug.Log("Compile started...");
 
         // Receives the time since editor began compiling the frame the editor compiles
-        m_dCompileStartTime = EditorApplication.timeSinceStartup;
+        m_dCompileStartTime = EditorApplication.timeSinceStartup; 
     }
 
     //--------------------------------------------------------------------------------
@@ -72,7 +91,28 @@ public class CompileTime : MonoBehaviour
         // Calculates the final compile time on the frame the editor stops compiling
         m_dFinalCompileTime = EditorApplication.timeSinceStartup - m_dCompileStartTime;
 
+        // Calls function that writes the passed in compile time to the text document
+        WriteToNotepad(m_dFinalCompileTime);
+
         // Logs the compile time to debug console, rounding to two decimal places
         Debug.Log("Compile Finished: " + m_dFinalCompileTime.ToString("F2") + "s");
+    }
+
+    //--------------------------------------------------------------------------------
+    // Function writes the compile time result to the text document in Resources.
+    //
+    // Param:
+    //      dCompileTime: A double that gets written to text file in the function.
+    //--------------------------------------------------------------------------------
+    public void WriteToNotepad(double dCompileTime)
+    {
+        // Initializes a StreamWriter to the text document
+        StreamWriter writer = new StreamWriter(m_sDirectory, true);
+        
+        // Writes the compile time passed in and rounds it to two decimal places
+        writer.WriteLine(dCompileTime.ToString("F2"));  
+
+        // Closes the stream to the text document
+        writer.Close();
     }
 } 
--- SD4ProgramCode/Assets/Scripts/MathFunctions.cs
+++ SD4ProgramCode/Assets/Scripts/MathFunctions.cs
@@ -1,12 +1,183 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
+[ExecuteInEditMode]
 public class MathFunctions : MonoBehaviour
 {
+    [Header("Clamp Function")]
+    public bool m_bClampFunction;
+
+    public float m_fClampValue;
+
+    public float m_fClampMin;
+
+    public float m_fClampMax;
+
+    [Header("Approximately Function")]
+    public bool m_bApproximatelyFunction;
+
+    public float m_fApproxA;
+
+    public float m_fApproxB;
+
+    [Header("DeltaAngle Function")]
+    public bool m_bDeltaAngleFunction;
+
+    public float m_fAngleCurrent;
+
+    public float m_fAngleTarget;
+
+    [Header("Lerp Function")]
+    public bool m_bLerpFunction;
+
+    public Vector3 m_v3LerpVectorA;
+
+    public Vector3 m_v3LerpVectorB;
+
+    public float m_fLerpValue;
+
+    [Header("Lerp Unclamped Function")]
+    public bool m_bLerpUnclampedFunction;
+
+    public Vector3 m_v3ULerpVectorA;
+
+    public Vector3 m_v3ULerpVectorB;
+
+    public float m_fULerpValue;
+
+    [Header("Move Towards Function")]
+    public bool m_bMoveTowardsFunction;
+
+    public Vector3 m_v3MoveCurrent;
+
+    public Vector3 m_v3MoveTarget;
+
+    public float m_fMoveValue;
+
+    [Header("Look At Function")]
+    public bool m_bLookAtFunction;
+
+    public Vector3 m_v3LookAtTarget;
+
+    [Header("Transform Vector Function")]
+    public bool m_bSmoothDampFunction;
+
+    public Vector3 m_v3DampCurrent;
+
+    public Vector3 m_v3DampTarget;
+
+    public Vector3 m_v3DampVelocity;
+
+    public float m_fDampTime;
+
+    [Header("Log Function")]
+    public bool m_bLogFunction;
+
+    public float m_fLogValue; 
+
+    public float m_fLogPower; 
+
+    [Header("Random Function")]
+    public bool m_bRandomFunction;
+
+    public float m_fRandomMin;
+
+    public float m_fRandomMax;  
+
+
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        
+        if (m_bClampFunction) 
+        {
+            Clamp(m_fClampValue, m_fClampMin, m_fClampMax);
+        }
+
+        if (m_bApproximatelyFunction)
+        {
+            Approximately(m_fApproxA, m_fApproxB);
+        }
+
+        if (m_bDeltaAngleFunction)
+        {
+            DeltaAngle(m_fAngleCurrent, m_fAngleTarget); 
+        }
+
+        if (m_bLerpFunction)
+        {
+            Lerp(m_v3LerpVectorA, m_v3LerpVectorB, m_fLerpValue); 
+        }
+
+        if (m_bLerpUnclampedFunction)
+        {
+            LerpUnclamped(m_v3ULerpVectorA, m_v3ULerpVectorB, m_fULerpValue); 
+        }
+
+        if (m_bMoveTowardsFunction)
+        {
+            MoveTowards(m_v3MoveCurrent, m_v3MoveTarget, m_fMoveValue);   
+        }
+
+        if (m_bLookAtFunction)
+        {
+            LookAt(m_v3LookAtTarget);
+        }
+
+        if (m_bSmoothDampFunction)
+        {
+            SmoothDamp(m_v3DampCurrent, m_v3DampTarget, m_v3DampVelocity, m_fDampTime);  
+        }
+
+        if (m_bLogFunction)
+        {
+            Mathf.Log(m_fLogValue, m_fLogPower);
+        }
+
+        if (m_bRandomFunction)
+        {
+            Random.Range(m_fRandomMin, m_fRandomMax);  
+        }
+    }
+
+    private void Clamp(float fValue, float fMin, float fMax)
+    {
+        Mathf.Clamp(fValue, fMin, fMax);
+    }
+
+    private void Approximately(float fApproxA, float fApproxB)
+    {
+        Mathf.Approximately(fApproxA, fApproxB);
+    }
+
+    private void DeltaAngle(float fAngleCurrent, float fAngleTarget)
+    {
+        Mathf.DeltaAngle(fAngleCurrent, fAngleTarget);
+    }
+
+    private void Lerp(Vector3 v3LerpVectorA, Vector3 v3LerpVectorB, float fLerpValue)
+    {
+        Vector3.Lerp(v3LerpVectorA, v3LerpVectorB, fLerpValue);
+    }
+
+    private void LerpUnclamped(Vector3 v3ULerpVectorA, Vector3 v3ULerpVectorB, float fULerpValue)
+    {
+        Vector3.LerpUnclamped(v3ULerpVectorA, v3ULerpVectorB, fULerpValue);
+    }
+
+    private void MoveTowards(Vector3 v3MoveCurrent, Vector3 v3MoveTarget, float fMoveValue)
+    {
+        Vector3.MoveTowards(v3MoveCurrent, v3MoveTarget, fMoveValue);
+    }
+
+    private void LookAt(Vector3 v3LookAtTarget)
+    {
+        transform.LookAt(v3LookAtTarget);
+    }
+
+    private void SmoothDamp(Vector3 v3DampCurrent, Vector3 v3DampTarget, Vector3 v3DampVelocity, float fDampTime)
+    {
+        Vector3.SmoothDamp(v3DampCurrent, v3DampTarget, ref v3DampVelocity, fDampTime);
     }
 }
